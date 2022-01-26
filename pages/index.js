d9eaf4e7d8c33aeb06d7e46e2915c7bb2a93d48f
evansouzaa <evandro.souza.pereira@gmail.com>
2022-01-25 22:33:38 -0300

    Implementado não permitir menos de 2 caracteres no input

--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,6 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    );
-}
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -67,7 +39,6 @@ export default function PaginaInicial() {
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -128,7 +99,7 @@ export default function PaginaInicial() {
                             fullWidth
                             onChange={function(event){
                                 const valor = event.target.value;
-                                setUsername(valor)
+                                if(valor.length > 2) setUsername(valor)
                             }}
                             textFieldColors={{
                                 neutral: {
